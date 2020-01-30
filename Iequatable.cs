0c1c356085c412f6b2bf2cec5a78c0141fa3d65c
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-30 14:03:09 +0530

    Add files via upload

---
+++ Iequatable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iequatable
+{
+    class Iequatable
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Pranshika";
+            string s2 = String.Copy(s1);
+            int num1 = 3;
+            int num2 = 5;
+            Console.WriteLine(num1.Equals(num2));
+            Console.WriteLine(s1.Equals(s2));
+        }
+    }
+}
