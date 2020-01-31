5baa8aeb1cb7973f420bb762784e136cf54bc3a6
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-31 13:24:35 +0530

    Add files via upload

---
+++ Iequatable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iequatable
+{
+    public class Employee : IEquatable<Employee>
+    {
+        public string Account;
+        public string Name;
+
+        public Employee(string account, string name)
+        {
+            this.Account = account;
+            this.Name = name;
+        }
+
+        public bool Equals(Employee other)
+        {
+            if (other == null) 
+                return false;
+            return (this.Account.Equals(other.Account));
+        }
+    }
+    class Iequatable
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("HDFC", "Pranshika");
+            Employee emp2 = new Employee("HDFC", "Ankur");
+            Console.WriteLine(emp1.Equals(emp2));
+        }
+    }
+}
